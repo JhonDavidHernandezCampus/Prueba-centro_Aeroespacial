58b9e6b4045c733007542a03f804d3c85f242ecf
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-05-11 11:56:58 -0500

    feat: :sparkles: avance en la consulta de las fechas

--- storage/wsConsu.js
+++ storage/wsConsu.js
@@ -1,13 +1,24 @@
 import datos from '../api/reclutas.js';
-let fecha = new Date('')
+let fecha = new Date();
+let f_hadosmeses = new Date(fecha.setDate(fecha.getDate()-60));
+
 
 let wsConsu = {
     async pintarConsulta(e){
         let data = await datos.consultas(e);
         
         console.log(data);
-        console.log(fecha);
+        console.log(f_hadosmeses);
+        console.log();
+        
         data.forEach(e => {
+            if (new Date(e.f_ingreso) < f_hadosmeses ) {
+                
+            }
+
+
+            console.log(new Date(e.f_ingreso), "fecha del recluta  "+ e.nombre);
+            console.log(f_hadosmeses,"fecha hace dos meses");
 
         });
     }
