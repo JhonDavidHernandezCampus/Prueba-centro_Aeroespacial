369c82d754c2a15c3a0a323dd1ef45fbf608633e
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-05-11 14:02:42 -0500

    feat: :sparkles: se termna la conslta 1

--- storage/consultas.js
+++ storage/consultas.js
@@ -1,10 +1,16 @@
 
 const op = document.querySelector("select");
-
+const datos = document.querySelector(".datos");
+const tabla = document.querySelector(".tabla");
 op.addEventListener("change",(e)=>{
+    tabla.style.display="block";
     const ws = new Worker('./storage/wsConsu.js', {type:"module"})
     ws.postMessage({module:"pintarConsulta" ,data:url()})
     
+    ws.addEventListener("message", (e)=>{
+        datos.innerHTML= e.data;
+
+    })
 
 })
 
@@ -13,7 +19,10 @@ function url(){
         case 1:
             return `http://localhost:3000/reclutas/`; 
         case 2:
-            return `http://localhost:3000/reclutas/`; 
+            let valor = document.querySelector("#valor");
+            valor.style.display= "block"
+            console.log(valor);
+            return `http://localhost:3000/reclutas?nombre=eddy`; 
         case 3:
             return `http://localhost:3000/reclutas/`; 
         case 4:
--- storage/wsConsu.js
+++ storage/wsConsu.js
@@ -1,26 +1,31 @@
 import datos from '../api/reclutas.js';
 let fecha = new Date();
-let f_hadosmeses = new Date(fecha.setDate(fecha.getDate()-60));
-
+let f_hadosmeses = new Date(fecha.setMonth(fecha.getMonth()-2));
 
 let wsConsu = {
     async pintarConsulta(e){
+        let ret="";
         let data = await datos.consultas(e);
-        
-        console.log(data);
-        console.log(f_hadosmeses);
-        console.log();
-        
-        data.forEach(e => {
-            if (new Date(e.f_ingreso) < f_hadosmeses ) {
-                
-            }
 
-
-            console.log(new Date(e.f_ingreso), "fecha del recluta  "+ e.nombre);
-            console.log(f_hadosmeses,"fecha hace dos meses");
-
-        });
+        let meses = data.filter(e =>{
+            return (new Date(e.f_ingreso) < f_hadosmeses)
+        }) 
+        let pintar = ((meses.length ==0)?data:meses)
+        pintar.forEach(e => {
+            ret += `
+            <tr>
+                <th>${e.nombre}</th>
+                <th>${e.edad}</th>
+                <th>${e.numero}</th>
+                <th>${e.email}</th>
+                <th>${e.direccion}</th>
+                <th>${e.f_nacimiento}</th>
+                <th>${e.num_id}</th>
+                <th>${e.f_ingreso}</th>
+                <th>${e.id}</th>
+            </tr>`;
+        }); 
+        return ret;
     }
 }
 
